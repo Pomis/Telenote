cecf0b00b921470285b6161ad9248d4cf1419ee6
Pomis <pomis172@gmail.com>
2017-05-31 06:24:49 +0300

    Notes adapter

--- app/src/main/java/pomis/app/telenote/adapters/NotesAdapter.kt
+++ app/src/main/java/pomis/app/telenote/adapters/NotesAdapter.kt
@@ -1,18 +1,43 @@
 package pomis.app.telenote.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
+import org.jetbrains.anko.find
+import pomis.app.telenote.R
 import pomis.app.telenote.models.Note
 
 /**
  * Created by romanismagilov on 31.05.17.
  */
-class NotesAdapter(context: Context?, resource: Int, textViewResourceId: Int, objects: MutableList<Note>?) : ArrayAdapter<Note>(context, resource, textViewResourceId, objects) {
-    
+class NotesAdapter(context: Context?, resource: Int, textViewResourceId: Int, var list: MutableList<Note>?)
+    : ArrayAdapter<Note>(context, resource, textViewResourceId, list) {
+
+    private var mInflator: LayoutInflater? = LayoutInflater.from(context)
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        return super.getView(position, convertView, parent)
+        val view: View
+        val vh: ViewHolder
+        if (convertView == null) {
+            view = mInflator!!.inflate(R.layout.item_note, parent, false)
+            vh = ViewHolder(
+                    view.find(R.id.tv_row_title),
+                    view.find(R.id.tv_row_description)
+            )
+            view.tag = vh
+        } else {
+            view = convertView
+            vh = view.tag as ViewHolder
+        }
+
+
+        vh.tvRowTitle.text = list!![position].title
+        vh.tvRowDescription.text = list!![position].description
+        return view
     }
+
+    class ViewHolder(var tvRowTitle: TextView, var tvRowDescription: TextView)
 }
--- app/src/main/java/pomis/app/telenote/fragments/NoteListFragment.kt
+++ app/src/main/java/pomis/app/telenote/fragments/NoteListFragment.kt
@@ -6,16 +6,41 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
+import org.jetbrains.anko.find
 
 import pomis.app.telenote.R
+import pomis.app.telenote.adapters.NotesAdapter
+import pomis.app.telenote.models.Note
 
 
 class NoteListFragment : Fragment() {
 
+    var notes: ListView? = null
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
         return inflater!!.inflate(R.layout.fragment_note_list, container, false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        initNotesList()
+    }
+
+    internal fun initNotesList() {
+        notes = activity.findViewById(R.id.ll_notes) as ListView?
+
+        refreshAdapter()
+    }
+
+    internal fun refreshAdapter() {
+        val note1 = Note()
+        note1.title = "kek123"
+        note1.description = "dfsfsdfsd"
+
+        val list = mutableListOf<Note>(note1, note1, note1, note1, note1, note1, note1, note1, note1)
+        notes!!.adapter = NotesAdapter(context, 0, 0, list)
     }
 
 }
