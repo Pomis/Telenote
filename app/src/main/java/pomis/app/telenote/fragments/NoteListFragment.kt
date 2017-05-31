f4ea7f20f3d68984f7c50283f928bd955377a861
Pomis <pomis172@gmail.com>
2017-05-31 03:48:10 +0300

    init

---
+++ app/src/main/java/pomis/app/telenote/fragments/NoteListFragment.kt
@@ -0,0 +1,25 @@
+package pomis.app.telenote.fragments
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import pomis.app.telenote.R
+
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class NoteListFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater!!.inflate(R.layout.fragment_note_list, container, false)
+    }
+
+}// Required empty public constructor
---
+++ app/src/main/java/pomis/app/telenote/models/Note.kt
@@ -0,0 +1,27 @@
+package pomis.app.telenote.models
+
+
+import io.realm.RealmObject
+import io.realm.annotations.RealmClass
+
+@RealmClass
+open class Note : RealmObject(){
+
+    open var path: String? = null
+
+    open var url: String? = null
+
+    open var title: String? = null
+
+    open var description: String? = null
+
+    open var author_name: String? = null
+
+    open var content: Content? = null
+
+    open class Content : RealmObject(){
+
+        open var children: ArrayList<String>? = null
+
+    }
+}
