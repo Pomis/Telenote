209ec045b238e2ae3d2faf06207cc9c888c212ee
Pomis <pomis172@gmail.com>
2017-06-01 03:02:22 +0300

    Java init

---
+++ app/src/main/java/pomis/app/telenote/PagesActivity.java
@@ -0,0 +1,14 @@
+package pomis.app.telenote;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PagesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pages);
+
+    }
+}
